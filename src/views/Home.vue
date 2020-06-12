941c65c1ecff76440afb930f584d113238ec22e3
Iago <iagofalcom@hotmail.com>
2020-06-12 08:55:47 -0400

    Gerador de qrcode com lib QRious

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <h1>{{title}}</h1>
+        <div>
+            <input type="text" size="25"
+                   placeholder="Digite para gerar..."
+                   v-model="text"
+            >
+        </div>
+        <div v-if="text" class="output">
+            <img :src="newQrcode" alt="QRCode">
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import QRious from 'qrious';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                title: 'Gerador de QrCode',
+                text: 'https://www.google.com.br',
+                qrcode: new QRious({size: 300})
+
+            }
+        },
+        computed: {
+            newQrcode() {
+                this.qrcode.value = this.text
+                return this.qrcode.toDataURL()
+            }
+        },
+        methods: {
+
+        }
+
+    }
 </script>
+
+<style>
+
+    h1{
+        color: white;
+    }
+
+    div.home{
+        background: #2c3e50;
+        height: 100vh;
+    }
+
+    div.output {
+
+        height: 400px;
+        width: auto;
+
+    }
+
+    div img{
+        background: white;
+        margin: 10px;
+        border: 5px solid white;
+    }
+
+</style>
